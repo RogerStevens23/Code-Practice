9e8d59f93d945bc33d31457f82ead0331f72462f
RogerStevens23 <stevens.roger23@gmail.com>
2022-04-26 23:56:44 -0400

    Practice with abstract classes and methods. Using the Animal Kingdom as an example.

---
+++ main/java/AnimalHierarchy/Animal_A.java
@@ -0,0 +1,13 @@
+package AnimalHierarchy;
+
+public abstract class Animal_A
+{
+    public abstract void move();
+    public abstract void eat();
+
+
+    public void label()
+    {
+        System.out.println("Animal's data:");
+    }
+}
---
+++ main/java/AnimalHierarchy/Bird.java
@@ -0,0 +1,13 @@
+package AnimalHierarchy;
+
+public class Bird extends Animal_A
+{
+    public void move()
+    {
+        System.out.println("Moves by flying.");
+    }
+    public void eat()
+    {
+        System.out.println("Eats bird food.");
+    }
+}
