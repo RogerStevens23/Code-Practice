f1efc0b44db16da9bf1be12f7b549870eab5de01
RogerStevens23 <stevens.roger23@gmail.com>
2022-09-06 01:31:35 -0400

    Added ability to create a new MenuScreen without writing a new class.

---
+++ main/java/MenuScreen/Screens/QuickScreen.java
@@ -0,0 +1,10 @@
+package MenuScreen.Screens;
+
+import MenuScreen.MenuScreen;
+import MenuScreen.Keyword;
+
+public class QuickScreen extends MenuScreen {
+    public QuickScreen(Keyword screenWord, Keyword...keywords) {
+        super(screenWord, keywords);
+    }
+}
