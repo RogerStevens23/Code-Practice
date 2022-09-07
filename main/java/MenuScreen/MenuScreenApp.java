cb7b4e460440d8e579e64a139b3f03257ce531ee
RogerStevens23 <stevens.roger23@gmail.com>
2022-09-07 06:15:49 -0400

    Updated the way input is collected, with a new method loopUserInputUntilCorrectKeyword. This returns the keyword the user finally selects. Also updated the logic to support entering the QuitScreen from any screen, as well as returning to a previous screen.

--- main/java/MenuScreen/MenuScreenApp.java
+++ main/java/MenuScreen/MenuScreenApp.java
@@ -1,57 +1,85 @@
 package MenuScreen;
 
+import MenuScreen.Screens.QuitScreen;
+
 import java.util.Scanner;
 
 public class MenuScreenApp
 {
     // Private Fields
-    private MenuScreen[] menuScreens;
-    private MenuScreen currentMenuScreen = null;
+    private final MenuScreen[] menuScreens;
+    private MenuScreen currentMenuScreen;
+    private MenuScreen previousMenuScreen;
 
     // Constructors
     public MenuScreenApp(MenuScreen[] menuScreens, int startScreen) {
         this.menuScreens = menuScreens;
         currentMenuScreen = menuScreens[startScreen];
+        previousMenuScreen = menuScreens[startScreen];
     }
     // Methods
     public void runScreen() {
         boolean quit = false;
         String userInput = "";
         while(!quit) {
+            // Display Current Screen
             currentMenuScreen.displayScreen();
-            userInput = getUserString();
-            if(!userInput.equalsIgnoreCase("Quit"))
-                currentMenuScreen = checkInputReturnScreen(userInput);
+            // Get user input
+            // Check if user input is valid
+            Keyword temp = loopUserInputUntilCorrectKeyword(currentMenuScreen);
+            // if not previous screen,
+            if(!temp.getWord().equalsIgnoreCase("Back")) {
+                // set previous screen to current screen
+                // set current screen to new screen
+                previousMenuScreen = currentMenuScreen;
+                currentMenuScreen = returnMenuScreenFromKeyword(temp);
+            }
+            // else set current screen to previous screen
             else
-                quit = true;
-        }
-    }
-    public MenuScreen checkInputReturnScreen(String userInput) {
-        boolean correct = false;
-        MenuScreen result = null;
-        while(!correct) {
-            if(!userInput.equalsIgnoreCase("quit")) {
-                if(currentMenuScreen.isCorrectKeyword(userInput)) {
-                    result = returnMenuScreenFromKeyword(currentMenuScreen.returnCorrectKeyword(userInput));
-                    correct = true;
-                }
-            }
-            else {
-                correct = true;
+                currentMenuScreen = previousMenuScreen;
+
+            if(currentMenuScreen instanceof QuitScreen) {
+                currentMenuScreen.displayScreen();
+                userInput = getUserString();
+                if(((QuitScreen) currentMenuScreen).isQuit(userInput))
+                    quit = true;
+                else
+                    currentMenuScreen = previousMenuScreen;
             }
         }
-        return result;
     }
+
     public MenuScreen returnMenuScreenFromKeyword(Keyword keyword) {
         MenuScreen result = null;
-        for(MenuScreen screen : menuScreens) {
-            if(screen.getScreenWord().getWord().equalsIgnoreCase(keyword.getWord()))
-                result = screen;
+        if(!keyword.getWord().equalsIgnoreCase("Quit")) {
+            for(MenuScreen screen : menuScreens) {
+                if(screen.getScreenWord().getWord().equalsIgnoreCase(keyword.getWord())) {
+                    result = screen;
+                    break;
+                }
+            }
         }
+        else
+            result = new QuitScreen();
         return result;
     }
 
     // User input Methods
+    public Keyword loopUserInputUntilCorrectKeyword(MenuScreen currentMenuScreen) {
+        boolean isCorrect = false;
+        Keyword result = null;
+        String userInput = "";
+        while(!isCorrect) {
+            userInput = getUserString();
+            if(currentMenuScreen.isCorrectKeyword(userInput)) {
+                result = currentMenuScreen.returnCorrectKeyword(userInput);
+                isCorrect = true;
+            }
+            else
+                System.out.println("Please enter a correct option!");
+        }
+        return result;
+    }
     public String getUserString() {
         Scanner input = new Scanner(System.in);
         return input.nextLine();
