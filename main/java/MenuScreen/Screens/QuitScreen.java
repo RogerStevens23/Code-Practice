d5fed5ad1182ee2643b8b7b11272570f79b51bfe
RogerStevens23 <stevens.roger23@gmail.com>
2022-09-07 06:11:26 -0400

    Added a new screen for quiting.

--- main/java/MenuScreen/Screens/QuitScreen.java
+++ main/java/MenuScreen/Screens/QuitScreen.java
@@ -1,2 +1,13 @@
-package MenuScreen.Screens;public class QuitScreen {
+package MenuScreen.Screens;
+
+import MenuScreen.MenuScreen;
+import MenuScreen.Keyword;
+
+public class QuitScreen extends MenuScreen {
+    public QuitScreen() {
+        super(new Keyword("Quit"), new Keyword("Yes"), new Keyword("No"));
+    }
+    public boolean isQuit(String user) {
+        return user.equalsIgnoreCase("Yes");
+    }
 }
