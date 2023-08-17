01561379f5df338f93e307b79e73842323804eec
Roger <Stevens.roger23@gmail.com>
2023-08-17 12:26:41 -0400

    Temp changes for practice classes.

--- main/java/CodePracticeApp.java
+++ main/java/CodePracticeApp.java
@@ -1,21 +1,28 @@
-import MenuScreen.MenuScreen;
-import MenuScreen.MenuScreenApp;
-import MenuScreen.Screens.QuickScreen;
-import MenuScreen.Keyword;
-import MenuScreen.Screens.TestScreen;
+import CodingBatPracticeClasses.Array1;
+import LeetCode.LeetCode75;
 
 
 public class CodePracticeApp {
 	public static void main(String[] args) { 
-		MenuScreen[] screens = new
-		MenuScreen[] { new QuickScreen(new Keyword("Start Screen"), new
-		Keyword("One"), new Keyword("Two"), new Keyword("Three")), new
-		QuickScreen(new Keyword("One"), new Keyword("Bob"), new Keyword("John"), new
-		Keyword("Paul")), new QuickScreen(new Keyword("Two"), new
-		Keyword("Start Screen")), new QuickScreen(new Keyword("Three")), new
-		QuickScreen(new Keyword("Bob"), new Keyword("Fish"), new Keyword("Bish"), new
-		Keyword("Dish")), new QuickScreen(new Keyword("John"), new Keyword("Yoko"),
-		new Keyword("One")), new QuickScreen(new Keyword("Yoko")), }; MenuScreenApp
-		App = new MenuScreenApp(screens, 0); App.runScreen();
+		// MenuScreen[] screens = new
+		// MenuScreen[] { new QuickScreen(new Keyword("Start Screen"), new
+		// Keyword("One"), new Keyword("Two"), new Keyword("Three")), new
+		// QuickScreen(new Keyword("One"), new Keyword("Bob"), new Keyword("John"), new
+		// Keyword("Paul")), new QuickScreen(new Keyword("Two"), new
+		// Keyword("Start Screen")), new QuickScreen(new Keyword("Three")), new
+		// QuickScreen(new Keyword("Bob"), new Keyword("Fish"), new Keyword("Bish"), new
+		// Keyword("Dish")), new QuickScreen(new Keyword("John"), new Keyword("Yoko"),
+		// new Keyword("One")), new QuickScreen(new Keyword("Yoko")), }; MenuScreenApp
+		// App = new MenuScreenApp(screens, 0); App.runScreen();
+
+		//System.out.println(LeetCode75.mergeAlterately("abc", "pqr"));
+		//System.out.println("Result is : " + LeetCode75.gcdOfString("ABABAB", "ABAB"));
+		
+		/* System.out.println("Results are: ");
+		int[] t = Array1.makeLast(new int[]{1,2,3});
+		for(int n : t) {
+			System.out.println(n);
+		} */
+		System.out.println("Results are: "+Array1.double23(new int[]{3,3}));
 	} 
 }
