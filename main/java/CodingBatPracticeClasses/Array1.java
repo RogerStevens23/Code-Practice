464c16b8d7764f9ee61b1d7aff3a5dd1705aa490
Roger <Stevens.roger23@gmail.com>
2023-08-17 20:07:46 -0400

    Completed two more practice problems.

--- main/java/CodingBatPracticeClasses/Array1.java
+++ main/java/CodingBatPracticeClasses/Array1.java
@@ -14,4 +14,25 @@ public class Array1 {
         }
         return result;
     }
+    /* Given an int array length 3, if there is a 2 in the array immediately followed by a 3,
+    set the 3 element to 0. Return the changed array. */
+    public static int[] fix23(int[] nums) {
+        for(int i=0; i < nums.length; i++) {
+            if(i <= nums.length-1)
+                if(nums[i] == 2 && nums[i+1] == 3)
+                    nums[i+1] = 0;
+        }
+        return nums;
+    }
+
+    /* Start with 2 int arrays, a and b, of any length. 
+    Return how many of the arrays have 1 as their first element. */
+    public static int start1(int[] a, int[] b) {
+        int result = 0;
+        if(a.length > 0 && a[0] == 1)
+            result++;
+        if(b.length > 0 && b[0] == 1)
+            result++;
+        return result;
+    }
 }
