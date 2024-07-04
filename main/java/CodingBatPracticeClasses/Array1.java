49eb7ecafee88583e8defad8c3816f3a853e0b6a
Roger <Stevens.roger23@gmail.com>
2024-07-04 16:44:11 -0400

    Finished another practice problem.

--- main/java/CodingBatPracticeClasses/Array1.java
+++ main/java/CodingBatPracticeClasses/Array1.java
@@ -183,7 +183,34 @@ public class Array1 {
         make2([4, 5], [1, 2, 3]) → [4, 5]
         make2([4], [1, 2, 3]) → [4, 1]
         make2([], [1, 2]) → [1, 2]*/
-
+    
+    public static int[] make2(int[] a, int[] b) {
+    	switch (a.length) {
+    		case 2:
+    			return new int[]{a[0],a[1]};
+    		case 1:
+    			return new int[]{a[0],b[0]};
+    	};
+    	
+    	
+    	
+    	
+    	int[] n = new int[2];
+    	if(a.length >= 2) {
+    		n[0] = a[0];
+    		n[1] = a[1];
+    	}
+    	else if(a.length == 1) {
+    		n[0] = a[0];
+    		n[1] = b[0];
+    	}
+    	else {
+    		n[0] = b[0];
+    		n[1] = b[1];
+    	}
+    	
+    	return n;   	
+    }
 
 }
 
