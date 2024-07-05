bb6d78819227935d7919a78c183ceeb2e884806a
Roger <Stevens.roger23@gmail.com>
2024-07-05 15:26:21 -0400

    Finished Array1 exercises.

--- main/java/CodingBatPracticeClasses/Array1.java
+++ main/java/CodingBatPracticeClasses/Array1.java
@@ -211,7 +211,24 @@ public class Array1 {
     	
     	return n;   	
     }
+    
+    /* Given 2 int arrays, a and b, of any length, return a new array with the first element of each array. 
+    If either array is length 0, ignore that array.
+	front11([1, 2, 3], [7, 9, 8]) → [1, 7]
+	front11([1], [2]) → [1, 2]
+	front11([1, 7], []) → [1]*/
 
+    public static int[] front11(int[] a, int[] b) {
+    	if(a.length != 0) {
+    		if(b.length != 0)
+    			return new int[]{a[0],b[0]};
+    		return new int[]{a[0]};
+    	}
+    	else
+    		if(b.length != 0)
+    			return new int[]{b[0]};
+    	return new int[]{};
+    }
 }
 
 
